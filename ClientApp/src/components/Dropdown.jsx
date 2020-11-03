b352e7bab94f89d9d63c3fe56520b6e1b82af0e9
kswickramasinghe <kushaninfo@gmail.com>
2020-11-04 12:44:42 +1300

    Edit drop down options

--- ClientApp/src/components/Dropdown.jsx
+++ ClientApp/src/components/Dropdown.jsx
@@ -4,8 +4,11 @@ import { Dropdown } from 'semantic-ui-react'
 function DropdownManu(props) {
 
     const pages=[
-        {key:1, value:"4", text: "4"},
-        {key:2, value:"3", text: "3"}
+        {key:1, value:"5", text: "5"},
+        { key: 2, value: "10", text: "10" },
+        { key: 2, value: "15", text: "15" },
+        { key: 2, value: "20", text: "20" },
+        { key: 2, value: "25", text: "25" }
       ];
 
     function handleDropdown(event, {value}){
